358aeb34e7708bef4e5459977ac79e8f3b21dda1
Nishik Patel <nishik1@live.com>
2024-10-13 00:10:43 -0400

    Player Movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody2D body;
+    private Vector2 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        movement = movement.normalized;
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 targetVelocity = movement * moveSpeed;
+    }
+}
