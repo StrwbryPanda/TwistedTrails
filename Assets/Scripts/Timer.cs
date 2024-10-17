b089831887171e31ebf7b7b2dd04006dad825c11
alexfang452 <alexfang452@gmail.com>
2024-10-16 21:20:41 -0400

    Small changes
    - Changed line to Timer script. It should work with the timer now. However, it changes the text ("Timer:") to the actual timer when I tried it.

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 
 public class Timer : MonoBehaviour
 {
     public float timeLeft = 0;
     public bool isTimeRunning = true;
-    public Text timeText;
+    public TMP_Text timeText;
 
     // Start is called before the first frame update
     void Start()
